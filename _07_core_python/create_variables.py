d0975e7b429a2a5ed3976be6949045c2dd2038db
UM5224 <gudurvamseedharreddy1997@gmail.com>
2021-12-22 17:35:13 +0530

    fourth

---
+++ _07_core_python/create_variables.py
@@ -0,0 +1,26 @@
+_myName = 'vamsi'
+myNumber = 24
+myNumber2 = 24
+is_present = True
+
+print(id(_myName))
+
+print(id(myNumber))
+
+print(id(myNumber2))
+
+print(id(is_present))
+
+print(type(_myName))
+
+print(type(myNumber))
+
+print(type(is_present))
+
+
+print("my name is " + _myName)
+
+print('my number is ',myNumber)
+
+print(is_present)
+
---
+++ _07_core_python/varia_type.py
@@ -0,0 +1,38 @@
+# how memory allocate to variables in python
+'''
+data allocate to values not to variables
+
+same value can have different variables
+
+different variables with same value have same id
+
+'''
+
+# data types
+'''
+int:-countable numbers
+
+float:-numbers with decimals
+
+boolean:-true or false 
+
+string:-a sequence of character
+'''
+
+#write operation
+a = 6
+#allocate memory to variable a
+
+
+# read operation
+print(a)
+# retrieve the value of the variable from data location
+
+# rules for defining the variables
+
+
+'''
+1) cannot starts with numbers
+2) can only starts with alphabet or underscore
+3) cannot have special character
+'''
