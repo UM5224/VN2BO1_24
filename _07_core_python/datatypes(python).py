15353c4e6c40f147f04f6d75b88ccee9f6f22b10
UM5224 <gudurvamseedharreddy1997@gmail.com>
2021-12-27 17:30:21 +0530

    some

---
+++ _07_core_python/datatypes(python).py
@@ -0,0 +1,36 @@
+#Data types and fuction
+# integer -1,20
+# float-decimal value included (10.5,20.6375)
+# boolean - True or False
+# string - "name","place"
+
+a=5
+b=10.5
+is_ok = True
+place = "bangalore"
+
+# #functions
+
+# print - print the output in console
+# type - type of create_variable
+# id - print the id value of variable
+
+print(a)
+print(type(a))
+print(id(a))
+
+print(b)
+print(type(b))
+print(id(b))
+
+print(is_ok)
+print(type(is_ok))
+print(id(is_ok))
+
+print(place)
+print(type(place))
+print(id(place))
+
+
+
+
---
+++ _07_core_python/operators(python).py
@@ -0,0 +1,54 @@
+# operatorss
+
+# 1)Arithmetic operators
+a =10
+b=15
+print(a+b) #adition
+print(a-b) #substraction
+print(a*b)#multiplication
+print(a/b)#division
+print(a%b)#modulus
+print(a**b)#exponentional
+print(a//b)#floor division
+
+#comparision operators
+print(a==b)#equals two values
+print(a>b)#compare wheter a is greater
+print(a<b)#compare wheter a is lesser
+print(a!=b)#compare two valuse are same or not
+print(a>=b)#compare greater than or euals
+print(a<=b)#compare lesser than or euals
+
+#Assignment operators
+#assign the value to the variable
+
+c=12
+print(c)
+c+=2        # c= c+2
+print(c)
+c-=2
+print(c)
+c*=2
+print(c)
+c/=2
+print(c)
+c%=2
+print(c)
+
+# #logical operators
+# 1)AND  - true only when both true
+# 2)OR - false only when both false
+# 3)NOT - true to false ,false to true
+
+# #Membership operators
+# tells wheter a particular variable present in sequence or not
+i = {1,2,3}
+print(2 in i)
+print(4 not in i)
+
+#Identity
+# compares the memory location
+x=21
+y=21
+print(x is y)
+print(x is not y)
